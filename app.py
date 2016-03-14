6bcba45493a4c1c57e1677974c596be634b4ac5f
Neependra Khare <neependra.khare@gmail.com>
2016-03-14 21:50:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ redis = Redis(host=os.environ.get('REDIS_HOST', 'redis'), port=6379)
 def hello():
     visits = redis.incr('hits')
     
-    html = "<h3>Hello Bangalore !!</h3> <br/>" \
+    html = "<h3>Hello Bangalore - First Github Meetup !!</h3> <br/>" \
            "<h2>No. Hits:</2> {hits}<br/>"
            
     return html.format(hits=visits)
