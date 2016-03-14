d1a13e996528080809e704c82e69171a73ad0ed6
Neependra Khare <neependra.khare@gmail.com>
2016-03-14 11:18:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,12 @@ redis = Redis(host=os.environ.get('REDIS_HOST', 'redis'), port=6379)
 
 @app.route('/')
 def hello():
-    redis.incr('hits')
-    return 'Hello World ! I have been seen %s times.' % redis.get('hits')
+    visits = redis.incr('hits')
+    
+    html = "<h3>Hello World !!</h3> <br/>" \
+           "<h2>No. Hits:</2> {hits}<br/>"
+           
+    return html.format(hits=visits)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
